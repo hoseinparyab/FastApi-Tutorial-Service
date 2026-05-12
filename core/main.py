8c6d3ca61e7bb0425e2df63d234a0bc502f58770
Paryab Hosein <75984205+hoseinparyab@users.noreply.github.com>
2026-05-12 20:01:55 +0300

    Remove empty __init__.py file and add endpoints to retrieve a list of names and name details in main.py

--- core/main.py
+++ core/main.py
@@ -1,9 +1,27 @@
+from os import name
 from fastapi import FastAPI
 
 app = FastAPI()
+name_list =[
+    {"id" :1, "name" : "ali"},
+    {"id" :2, "name" : "hosein"},
+    {"id" :3, "name" : "hasan"},
+    {"id" :1, "name" : "reza"},
+    {"id" :1, "name" : "roozbeh"},
+]
 
+@app.get("/names")
+def retrive_name_list() :
+    return name_list
 
+@app.get("/names/{name_id}")
+def retrive_name_detail(name_id:int):
+    for name in name_list:
+        if name[id]== name_id:
+         return name
+    return {"object not found"}
+    # print(name_id)
+    # return()
 @app.get("/")
-def hello_world():
+def root():
     return {"message": "Hello World"}
-    
